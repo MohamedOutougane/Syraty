0e5be2df171dd414abf12417cc53c85a7412094c
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-27 22:51:45 +0100

    creating the schema for the post table

--- database/migrations/2023_01_27_212702_create_posts_table.php
+++ database/migrations/2023_01_27_212702_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Rating;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +17,15 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('body', 2500)->nullable()->default(null);
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
+            $table->boolean('public')->default(false);
             $table->timestamps();
+
+            $table->foreignId(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignId(Rating::class)->constrained()->cascadeOnDelete();
         });
     }
 

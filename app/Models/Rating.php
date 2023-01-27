8dc214deb35a1f893a4769e8f5a095b9b25a4b9a
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-27 23:19:27 +0100

    ajout de proprité guarded, pour definir les attributs qui ne doivent pas etre affecté en masse

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+
+    // La propriété $guarded pour attribut peuvent pas être affectés en masse. 
+    // Par défaut, tous les attributs de la table sont affectables en masse, 
+    // si envie limiter ça, je spécifie les attributs qui doivent pas être affectés 
+    // en masse en définissant la propriété $guarded.
+    protected $guarded = [];
 }

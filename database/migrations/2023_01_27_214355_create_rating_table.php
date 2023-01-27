809eed68f4c8efc5f92c436284418eeed71ce7e7
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-27 22:51:57 +0100

    creating the schema for the rating table

--- database/migrations/2023_01_27_214355_create_rating_table.php
+++ database/migrations/2023_01_27_214355_create_rating_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('rating', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->int();
             $table->timestamps();
         });
     }

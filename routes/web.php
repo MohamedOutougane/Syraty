65574f04d8bced73860c7adfa94af09025d53f18
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-28 10:08:42 +0100

    comment the base route

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::resource('posts', PostController::class)->except('index'); // add : " ->m
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');

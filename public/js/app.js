3c93bcc9970de45787a9a676f2c805bb5fa2ea73
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-29 14:53:54 +0100

    css and js editing (not finished)

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const formContainer = document.querySelector('.form-container');
+    const button = document.querySelector('.creator-button button');
+  
+    formContainer.style.display = 'none';
+  
+    button.addEventListener('click', function() {
+      formContainer.style.display = formContainer.style.display === 'none' ? 'block' : 'none';
+    });
+});
+
 import './bootstrap';
 
 import Alpine from 'alpinejs';
@@ -5,3 +16,4 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+

79142d6c5312d4d094e4b33aa80fe23de8a2335f
Outougane Mohamed <outougane.moh@gmail.com>
2023-01-29 16:19:28 +0100

    correction js function and call

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,24 @@
+// import './bootstrap';
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+// Ajout d'un écouteur d'événement sur le document qui se déclenche une fois le DOM complètement chargé.
 document.addEventListener("DOMContentLoaded", function() {
+    
+    // On récupère le formulaire.
     const formContainer = document.querySelector('.form-container');
+    // On récupère le bouton qui va déclencher l'affichage du formulaire.
     const button = document.querySelector('.creator-button button');
   
+    // On cache le formulaire.
     formContainer.style.display = 'none';
   
+    // On ajoute un écouteur d'événement sur le bouton qui va afficher/masquer le formulaire.
     button.addEventListener('click', function() {
       formContainer.style.display = formContainer.style.display === 'none' ? 'block' : 'none';
     });
 });
-
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -135,7 +135,7 @@
         @include('footer')
         </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="{{ asset('js/app.js') }}">
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
     

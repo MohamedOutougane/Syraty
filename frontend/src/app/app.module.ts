da88520418238f4b7ea95374aed5cd5d78af31da
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-07 16:32:15 +0100

    configuring the app.module to have the httpmodule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  { 
+    path: '',
+    component: PostsComponent 
+  }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +19,9 @@ import { PostsComponent } from './components/posts/posts.component';
     PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

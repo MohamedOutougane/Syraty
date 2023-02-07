45a16da1fecfcbbb1f2d816eee2f37abe0a81eb5
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-07 17:09:47 +0100

    catch and the log the data

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../service/data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  posts: any;
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.getPostsData();
+  }
+
+  getPostsData() {
+    console.log('liste des posts');
+    this.dataService.getData().subscribe(res => {
+      console.log(res);
+    });
+  }
 }

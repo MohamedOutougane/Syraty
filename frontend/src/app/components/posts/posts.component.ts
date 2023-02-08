98c4fb3c3d7a7009d6b4b70a9d74ad15e0073f8e
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-08 16:15:36 +0100

    i can insert data (BUT NOT IMAGES) and delete them

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -11,6 +11,7 @@ export class PostsComponent implements OnInit {
   posts: any;
   ratings: any;
   post=new Post();
+  imageFile: File | undefined;
 
   constructor(private dataService: DataService) { }
 
@@ -33,12 +34,30 @@ export class PostsComponent implements OnInit {
       this.ratings = res.ratings;
     });
   }
+  processFile(imageInput: any) {
+    const file = imageInput.files[0];
+    this.imageFile = file;
+  }
   insertData() {
     console.log('insertion d\'un post');
+    // this.post.image = file;
+    console.log(this.post.image);
     console.log(this.post);
-    // this.dataService.insertData().subscribe(res => {
-    //   console.log(res);
-    //   this.posts = res;
-    // });
+    console.log(this.imageFile);
+    this.post.image = this.imageFile;
+    this.dataService.insertData(this.post).subscribe(res => {
+      console.log(res);
+      // this.posts = res;
+      this.getPostsData();
+    });
   }
+  deleteData(id: any) {
+    console.log(id);
+    console.log('suppression d\'un post');
+    this.dataService.deleteData(id).subscribe(res => {
+      console.log(res);
+      this.getPostsData();
+    });
+  }
+  
 }
--- frontend/src/app/service/data.service.ts
+++ frontend/src/app/service/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Post } from '../post';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,13 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
   getData() {
     return this.httpClient.get('http://127.0.0.1:8000/api/posts');
+  }
+  insertData(data:Post) {
+    console.log('http://127.0.0.1:8000/api/addPost', data);
+    return this.httpClient.post('http://127.0.0.1:8000/api/addPost', data);
+  }
+  deleteData(id:any) {
+    console.log('http://127.0.0.1:8000/api/deletePost/'+ id);
+    return this.httpClient.delete('http://127.0.0.1:8000/api/deletePost/'+ id);
   }
 }

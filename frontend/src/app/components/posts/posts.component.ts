eeaca4f015e5576b2b31248bc6254a0a4d4f375b
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-08 10:33:18 +0100

    adding rating info to the form

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -8,18 +8,34 @@ import { DataService } from '../../service/data.service';
 })
 export class PostsComponent implements OnInit {
   posts: any;
+  ratings: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.getPostsData();
+    this.getRatingsData();
   }
 
   getPostsData() {
     console.log('liste des posts');
-    this.dataService.getData().subscribe(res => {
-      console.log(res);
-      this.posts = res;
+    this.dataService.getData().subscribe((res: any) => {
+      console.log(res.posts);
+      this.posts = res.posts;
     });
+  }
+  getRatingsData() {
+    console.log('liste des ratings');
+    this.dataService.getData().subscribe((res: any) => {
+      console.log(res.ratings);
+      this.ratings = res.ratings;
+    });
+  }
+  insertData() {
+    console.log('insertion d\'un post');
+    // this.dataService.insertData().subscribe(res => {
+    //   console.log(res);
+    //   this.posts = res;
+    // });
   }
 }

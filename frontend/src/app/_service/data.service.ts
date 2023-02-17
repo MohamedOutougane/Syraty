4a35e2e7a75fd6a3dc8edd6f3c6393012848f252
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-17 19:22:36 +0100

    deleting console.log on the data services

--- frontend/src/app/_service/data.service.ts
+++ frontend/src/app/_service/data.service.ts
@@ -20,27 +20,21 @@ export class DataService {
     return this.httpClient.get(this.adresse + 'posts');
   }
   getData(id:any) {
-    console.log('http://127.0.0.1:8000/api/userPosts', id);
     return this.httpClient.get(this.adresse + 'userPosts/'+ id);
   }
   insertData(data:Post, httpOptions:any) {
-    console.log('http://127.0.0.1:8000/api/addPost', data, httpOptions);
     return this.httpClient.post(this.adresse + 'addPost', data);
   }
   deleteData(id:any, httpOptions:any) {
-    console.log('http://127.0.0.1:8000/api/deletePost/'+ id);
     return this.httpClient.delete(this.adresse + 'deletePost/'+ id, httpOptions);
   }
   loginUser(credentials:ICredential, httpOptions:any) {
-    console.log('http://127.0.0.1:8000/api/login', credentials);
     return this.httpClient.post<IuserReturn>(this.adresse + 'login', credentials);
   }
   registerUser(credentials:any, httpOptions:any) {
-    console.log('http://127.0.0.1:8000/api/login', credentials);
     return this.httpClient.post<IuserReturn>(this.adresse + 'register', credentials);
   }
   getSearchedData(search:any) {
-    console.log('http://127.0.0.1:8000/api/search/', search);
     return this.httpClient.post(this.adresse + 'search', search);
   }
 }
